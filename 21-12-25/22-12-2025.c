7a246c6bd4b5451bd6106e633d10cf6dd4f583e2
somisettyvenkateswarlu17-ops <somisettyvenkateswarlu17@gmail.com>
2025-12-22 20:27:27 +0530

    Add files via upload

---
+++ 21-12-25/22-12-2025.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!inta 
+    int a;
+    scanf("%d",&a);
+    if(a>=0) {
+        printf("postive");
+    }
+    else 
+    printf("negative");
+
+    
+    return 0;
+}
