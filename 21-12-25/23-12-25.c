c2af8173d952c4fe94d95752966f6c54df7d19b9
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2025-12-23 21:58:31 +0530

    Add files via upload

---
+++ 21-12-25/23-12-25.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+
+    int a;
+    scanf("%d",&a);
+    if(a>=40){
+        printf("passes");
+    }
+    else
+    printf("fail");
+    return 0;
+}
