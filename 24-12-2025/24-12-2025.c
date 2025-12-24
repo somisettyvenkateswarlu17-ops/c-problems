d2408bf130289df9e35bff72b5db0d555283709d
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2025-12-24 21:38:43 +0530

    Add files via upload

---
+++ 24-12-2025/24-12-2025.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a<b){
+        printf("%d",b);
+    }
+      else 
+        printf("%d",a);
+    
+    return 0;
+}
---
+++ 24-12-2025/24-12-25.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    float s;
+    int a;
+    scanf("%d",&a);
+    scanf("%f",&s);
+    if(a>=25&&s>=30000){
+        printf("e");
+    }
+    else
+    printf("not e");
+    return 0;
+}
