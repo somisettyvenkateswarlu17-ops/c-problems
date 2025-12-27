6e537967106b26cb44654061abdb8d4429066487
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2025-12-27 19:41:59 +0530

    Add files via upload

---
+++ 27-12-25.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=35){
+        printf("pass");
+
+    }
+    else if(a>=30){
+
+    
+    printf("grace pass");
+    }
+    else 
+    printf("fail");
+    
+    return 0;
+}
