46170083d07a3fa9abfc4295d3a67b92f6dca03c
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-01-01 19:19:29 +0530

    Add files via upload

---
+++ 01-01-2026.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int n;
+    int i;
+    int arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+
+    
+    return 0;
+}
