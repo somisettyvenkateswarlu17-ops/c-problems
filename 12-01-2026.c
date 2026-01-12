5b3707b081e19d46321a190ca169ae02bdb1f98b
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-01-12 22:38:13 +0530

    Add files via upload

--- 12-01-2026.c
+++ 12-01-2026.c
@@ -1,30 +1,12 @@
 #include <stdio.h>
-int main() {
-    //Your Code goes here!145
-    
-    int n;
-    scanf("%d",&n);
-    int sum=0;
-   int x=n;
-   while (n!=0){
-   int rem=n%10;
-   int fact=1;
-   for( int i=0; i<=rem; i++){
-     fact=fact*i;
-   }
-   n=n/10;
-   sum=sum+fact;
-
-
-   }
-   
-   printf("%d\n", sum);
-   if (sum==x){
-    printf("strong");
-   }
-   else {
-    printf("not_strong");
-   }
-    
-    return 0;
+int main(){
+   int n; 
+   int count=0; 
+   scanf("%d",&n);
+   for(int i=0;i<n;i++){
+     count++;
+    printf("%d\n ",count);
 }
+}
+    
+     
