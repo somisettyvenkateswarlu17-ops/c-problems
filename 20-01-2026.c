be9f0ea84a9f3a3bdb5c5b053b6e0710218bef24
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-01-20 21:55:32 +0530

    Add files via upload

---
+++ 20-01-2026.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int binary, decimal = 0, base = 1, rem;
+
+    
+    scanf("%d", &binary);
+
+    while (binary > 0) {
+        rem = binary % 10;      
+        decimal = decimal + rem * base;
+        binary = binary / 10;   
+        base = base * 2;
+    }
+
+    printf("Decimal equivalent = %d", decimal);
+
+    return 0;
+}
