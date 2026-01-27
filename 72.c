ecadfe6dbd7edd45541e5f0a51b5e5221f924c4e
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-01-27 20:02:51 +0530

    Add files via upload

---
+++ 72.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100], temp;
+    int i, j, len;
+    printf("Enter a string: ");
+    gets(str);
+    len = strlen(str);
+    for (i = 0; i < len - 1; i++) {
+        for (j = i + 1; j < len; j++) {
+            if (str[i] > str[j]) {
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+    printf("Sorted string: %s", str);
+    return 0;
+}
